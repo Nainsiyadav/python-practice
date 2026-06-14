f746a7436745864905a0e4c0e4ab05001c8a467c
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-14 10:36:32 +0530

    adding python program file

---
+++ comments.py
@@ -0,0 +1,11 @@
+
+# Comments 
+#  "#"  is used for SINGLE LINE COMMENT.
+print("nainsi yadav") # this is my name .
+
+# Multi-line comment
+# ('''  ''') is used for multiline comment.
+print("bhavan's college ")
+''' this is 
+my college name.  
+It  is located  in andheri west .'''
