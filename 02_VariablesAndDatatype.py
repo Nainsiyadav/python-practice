32f374680ffc79bff131b89f9f8d9a0c1f42c4ed
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-15 16:41:52 +0530

    adding variables and data type in python program

---
+++ 02_VariablesAndDatatype.py
@@ -0,0 +1,38 @@
+
+# -- Variables  --
+''' there are 3 rule for variable
+1. variables name can not start with numbers.
+2. No spaces allowed
+3. No special characters(except underscore).
+
+Examples---->
+'''
+a =  10  # (here 'a' is a variable) 
+print(a)
+
+name = "nainsi"
+print(name)
+
+#  Valid variable names
+my_name = "nainsi"
+age1 = 19
+_marks =  95
+
+# Invalid variables names
+# 1name = "a"  # start with number
+#  my name = "s" # contains space   
+# my-name = "r"  # contain special character
+
+
+
+#  ------Data-type-------
+
+age = 19 # int 
+height = 5.4 # float
+name = "sunita" #  String
+is_student = True # boolean
+
+print(age)
+print(height)
+print(name)
+print(is_student)
