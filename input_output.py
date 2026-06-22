0adffc2a1c4344a4d5adc0b1aa67fc074ac0b885
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-22 17:23:10 +0530

    updating python file

---
+++ input_output.py
@@ -0,0 +1,19 @@
+
+# Output - print()
+
+name = "Nainsi"
+age  = 18
+print("your name is",name,"and your age is",age)
+
+# Also we can use formated String for printing
+
+print(f"hi my name is {name} and my age is {age}")
+
+
+# input - input()
+
+name = input("please enter your name : ")
+age = int(input("please enter your age : "))
+print(f"hii your name is {name } and your age {age}")
+
+# NOTE : input always returns a string if you want number , convert it manually with int() or float().
--- type_conversion.py
+++ type_conversion.py
@@ -3,10 +3,43 @@
 
 # you can convert a  value from one type to another tpye using these built-in functions.
 
-
+# 1. Converting string value to int value
 a  =   "8"
 b = int(a) # tpye casting Strint To integer
 
 print(type(a))  # it will return 'str' (string)
 
 print(type(b))  # it will return 'int' (integer)
+
+#2. Converting int to Float 
+a=6
+print(float(a))
+print(type(a))
+
+# 3. You  can convert any data type to string value
+a=134
+b=56.8
+c=12+13j
+d=True
+
+print(str(a))
+print(str(b))
+print(str(c))
+print(str(d))
+
+# 4. Converting into boolean value
+
+a=12
+b=0
+c=12.6
+d=0.0
+e=""
+f="hello"
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))
+print(bool(e))
+print(bool(f))
+
