0d830056149a4e3c737dc38fae9cb617cff85cea
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-25 17:47:13 +0530

    adding some question in conditional file

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -27,3 +27,46 @@ elif(money==500):
     print("i will go mcd")
 else:
     print("i will stay hungry")
+
+
+
+# Quezzzz
+# Quezzz.1---->
+
+a = int(input("please enter your first number : "))
+b = int(input("please enter your second number :"))
+if(a>b):
+    print("a is greatest number ")
+elif(b>a):
+    print("b is greatest number ")
+else:
+    print("Both numbers are eqale ")
+
+# Quezzz.2------>
+
+gender = str(input("please enter your gender : "))
+if(gender=="M" or gender=="m"):
+    print("hlo! your are a male ")
+elif(gender=="F" or gender=="f"):
+    print("hlo! your are a female")
+else:
+    print("Sorry! i think you are transgender")
+
+# Quezzz.3--->
+
+n = int(input("please enter your number here : "))
+if(n%2==0):
+    print("your number is even ")
+else:
+    print("your number is odd ")
+
+
+# Quezzz.4----->
+
+year = int(input("please enter your year : "))
+if(year%100==0 and year%400==0):
+    print("your year is leap year")
+elif(year%100 !=100 and year%400==0):
+    print("your year is leap year")
+else:
+    print("your year is not a leap year")
